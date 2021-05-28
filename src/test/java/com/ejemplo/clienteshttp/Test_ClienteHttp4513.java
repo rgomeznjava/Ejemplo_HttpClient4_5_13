667c53dd62dd346c1b1fd14a98845793c239b93e
R2D2 <R2D2@192.168.1.13>
2021-05-28 03:27:15 +0200

    _

--- src/test/java/com/ejemplo/clienteshttp/Test_ClienteHttp4513.java
+++ src/test/java/com/ejemplo/clienteshttp/Test_ClienteHttp4513.java
@@ -1,8 +1,6 @@
 package com.ejemplo.clienteshttp;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
